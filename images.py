d66b88f1c3c706967427197e770add9eae465244
satwik2708 <81012408+satwik2708@users.noreply.github.com>
2021-03-31 10:14:15 +0530

    Add files via upload

---
+++ images.py
@@ -0,0 +1,18 @@
+image = \
+{ \
+	"logo" : "../static/twitter.png", \
+	"login" : "../static/login-rounded-right.png", \
+	"signup" : "../static/add-user-male.png", \
+	"home" : "../static/home-page.png", \
+	"post_tweet" : "../static/plus.png", \
+	"my_tweets" : "../static/stack-of-tweets.png", \
+	"users" : "../static/select-users.png", \
+	"read_tweets" : "../static/list.png", \
+	"network_analysis" : "../static/network-2-1.png", \
+	"company_analysis" : "../static/company.png", \
+	"account" : "../static/settings.png", \
+	"logout" : "../static/shutdown.png", \
+	"usr" : "../static/change-user-male.png", \
+	"" : ""
+}
+
